8862e0c786a0faba52ffd0c91571533eaaf81992
bala-amypo <bala@amypo.in>
2025-12-22 06:45:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/Contract.java
+++ src/main/java/com/example/demo/entity/Contract.java
@@ -2,37 +2,36 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 import java.math.BigDecimal;
-import java.time.*;
 
 @Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = "contractNumber"))
 public class Contract {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String contractNumber;
-
-    private String title;
-    private String counterpartyName;
-    private LocalDate agreedDeliveryDate;
     private BigDecimal baseContractValue;
-    private String status = "ACTIVE";
-
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-
-    @PrePersist
-    void onCreate() {
-        createdAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    void onUpdate() {
-        updatedAt = LocalDateTime.now();
-    }
 
     public Contract() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContractNumber() {
+        return contractNumber;
+    }
+
+    public BigDecimal getBaseContractValue() {
+        return baseContractValue;
+    }
+
+    public void setContractNumber(String contractNumber) {
+        this.contractNumber = contractNumber;
+    }
+
+    public void setBaseContractValue(BigDecimal baseContractValue) {
+        this.baseContractValue = baseContractValue;
+    }
 }
