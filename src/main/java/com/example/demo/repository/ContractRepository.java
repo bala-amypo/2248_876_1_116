4599a80fb38267a66b9d7b0aba9a302fe9659179
bala-amypo <bala@amypo.in>
2025-12-23 03:42:02 +0000

    User commit

--- src/main/java/com/example/demo/repository/ContractRepository.java
+++ src/main/java/com/example/demo/repository/ContractRepository.java
@@ -1,9 +1,8 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
+
+import com.example.demo.entity.Contract;
 
 public interface ContractRepository extends JpaRepository<Contract, Long> {
-    Optional<Contract> findByContractNumber(String contractNumber);
 }
